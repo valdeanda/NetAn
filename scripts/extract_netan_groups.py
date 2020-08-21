ad11b6b75bf0f726c6174d55ac89ceb1a455c4f8
Ian Rambo <ian@Ians-MacBook-Pro.local>
2020-08-21 15:36:58 -0500

    updated R visNetwork script, added extract_netan_groups.py, added netan osx-64 conda environment yml

---
+++ scripts/extract_netan_groups.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+#  General info ------------------------------------------
+
+"""
+Name: extract_netan_groups.py
+Purpose: Extract node group information from NetAn network output. 
+@uthor: Ian M Rambo - ian.rambo@utexas.edu, shrimpngrits44@gmail.com
+Created: July 1, 2020
+"""
+
+# Dependencies -------------------------------------------
+import re
+import os
+import argparse
+import logging
+
+def connected_components(line):
+    """
+    Extract connected component members and group number within the network.
+    """
+    if re.match(r'^- Nodes in CC_\d+:', line):
+        try:
+            line_list = line.split(':')
+            group_number = re.findall('Nodes in CC_(\d+):', line)[0]
+        except:
+            print('could not extract connected component group number')
+        nodes = (group_number, ['{}\t{}\n'.format(group_number, n) for n in line_list[1].strip().split(',')])
+        return nodes
+
+    else:
+        return None
+#=============================================================================
+#if __name__ == '__main__':
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-i', '--input', type=str, dest='infile', action='store',
+help='input NetAn network file')
+
+parser.add_argument('-o', '--output', type=str, dest='output', action='store',
+help='output NetAn groups file')
+
+parser.add_argument('-l', '--log', type=str, dest='log', action='store',
+help='logfile')
+
+
+args = parser.parse_args()
+
+#Set up logger
+logging_format = '%(levelname)s %(asctime)s - $(message)s'
+
+if args.log:
+    #Create the joblog directory if not specified
+    if not os.path.exists(os.path.dirname(args.log)):
+        os.makedirs(os.path.dirname(args.log))
+    else:
+        pass
+    logging.basicConfig(filename = args.log, level = logging.DEBUG, format = logging_format)
+else:
+    logfile_default = 'extract_netan_groups.{}.joblog'.format(str(datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
+    logging.basicConfig(filename = logfile_default,
+    level = logging.DEBUG, format = logging_format)
+
+logger = logging.getLogger()
+
+try:
+    input_handle = open(args.infile, 'r')
+    logger.info('opened input handle')
+except FileNotFoundError:
+    input_error = 'could not open input file {}'.format(args.infile)
+    logger.error(input_error)
+    print(input_error)
+
+try:
+    output_handle = open(args.output, 'w')
+    logger.info('opened output handle')
+except IOError:
+    output_error = 'could not write to output {}'.format(args.output)
+    logger.error(output_error)
+    print(output_error)
+
+#Loop through the file and extract connected component members
+#Write to output
+lnum = 0
+for line in input_handle:
+    line = line.rstrip()
+    if line.startswith('- Nodes in CC_'):
+        cc = connected_components(line)
+        if cc:
+            cc_num_msg = 'CC number {}'.format(cc[0])
+            cc_mem_msg = '{} members in CC number {}'.format(len(cc[1]), cc[0])
+            logger.info(cc_num_msg)
+            logger.info(cc_mem_msg)
+            for c in cc[1]:
+                output_handle.write(c)
+        else:
+            no_cc_msg = 'Line {}: found "Nodes in CC_", but returned no members'.format(lnum)
+            logger.info(no_cc_msg)
+    else:
+        continue
+    lnum += 1
+
+input_handle.close()
+logger.info('Closed input file')
+output_handle.close()
+logger.info('Closed output file')
--- scripts/plot_graph.R
+++ scripts/plot_graph.R
@@ -1,19 +1,82 @@
+#Name:     plot_graph.R
+#Purpose:  Visualize a NetAn network 
+#@uthor:  Valerie de Anda -vdeanda@ciencias.unam.mx - valdeanda@utexas.edu
+#Created:    September  2017
+#Modified: August 12, 2020 
+#Modified by: Ian Rambo - ian.rambo@utexas.edu - shrimpngrits44@gmail.com
 
-install.packages("visNetwork")
+#install.packages("visNetwork")
 #devtools::install_github("datastorm-open/visNetwork") #for development version
+
 library(visNetwork)
+library(tidyverse)
+library(RColorBrewer)
 
-nodes<-read.table("../data/nodes.txt", sep="\t",header=TRUE)
-edges<-read.table("../data/edges.txt", header = TRUE,sep="\t")
+create.network <- function(n, e, tmain, tsub, select_by, h = "700px", w = "100%") { 
+  ### Visualize a network using visNetwork
+  visNetwork(n, e, height = h, width = w,
+             main = tmain,
+             submain = tsub) %>%
+    visEdges(arrows="to") %>%
+    visOptions(selectedBy = select_by, 
+               highlightNearest = TRUE, 
+               nodesIdSelection = TRUE,
+               manipulation = TRUE) %>%
+    visPhysics(stabilization = FALSE) %>%
+    visConfigure(enabled = TRUE) }
 
+id.color.shape.df <- function(idv, idv_name) {
+  ### Dataframe of shapes and qualitative palette colors
+  ### This vector can be used in a dplyr::left_join()
+  ### The shapes and colors are used in VisNetwork
+  
+  node_shapes = c("square", "triangle", "box",
+                  "dot", "star", "ellipse", "diamond")
 
+  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+  
+  qual_col_vec = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+  
+  node_col_shape = data.frame(qual_col_vec, rep(node_shapes, length.out = length(qual_col_vec)))
+  colnames(node_col_shape) = c("color", "shape")
+  
+  idv = unique(idv)
+  incs = data.frame(idv,
+                    node_col_shape$color[1:length(idv)],
+                    node_col_shape$shape[1:length(idv)])
+  
+  colnames(incs) = c(idv_name, "color", "shape")
+  
+  return(incs)
+}
+#=============================================================================
+#Tu directorio 
+#Directory with node and edge files
+data_dir <- "~/development/NetAn/data/"
+#Output directory
+output_dir <- "~/development/NetAn/data/"
 
-visNetwork(nodes,edges, height = "700px", width = "100%",main = "Test", submain = "Only 0.05% of the total interactions"
-) %>%
-  visEdges(arrows="to")%>%
-  visOptions(selectedBy = "Domain", 
-             highlightNearest = TRUE, 
-             nodesIdSelection = TRUE,
-             manipulation=TRUE) %>%
-  visPhysics(stabilization =FALSE)%>%
-visConfigure(enabled=TRUE)
+#Necesites cambiar los nombres de los archivos
+#Your node and edge file names might differ - change accordingly
+nodes <- read.table(file.path(data_dir, "nodes_tax.tab"), sep = "\t", header = TRUE)
+edges <- read.table(file.path(data_dir, "edges.tab"), header = TRUE, sep="\t")
+
+edges$color <- vector(mode = "character", length = nrow(edges))
+
+#Añadir colores que corresponden con valores 
+edges$color[which(edges$Strength > 0)] <- "#FF7F00"
+edges$color[which(edges$Strength < 0)] <- "#386CB0"
+
+#Hace un dataframe que tiene figuras y colores para etiquetas taxonomicas
+tax_col_shape <- id.color.shape.df(nodes$Domain, "Domain")
+
+nodes_colshape <- nodes %>% left_join(tax_col_shape)
+
+vis_network <- create.network(nodes_colshape, edges,
+               tmain = "Only 0.05% of the total interactions", tsub = "Mi red chido",
+               select_by = "Phylum")
+
+network_name <- paste("visNetwork_", Sys.Date(), ".html", sep = '')
+network_html <- file.path(output_dir, network_name)
+
+visSave(vis_network, file = network_html)
